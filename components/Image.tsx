0df08856979eb60c85170e88a62a34ff3f53f805
Alex3236 <alex3236@qq.com>
2025-04-25 21:30:06 +0800

    doc: add bilibili-2024.mdx
    feat: add Gallery component and enhance SimpleImage with className prop

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,6 +1,7 @@
 import NextImage, { ImageProps } from 'next/image'
 import { PhotoView } from '@/components/PhotoPreviewerWrapper'
 import { ImgProps } from 'next/dist/shared/lib/get-img-props'
+import React from 'react'
 
 const basePath = process.env.BASE_PATH
 
@@ -13,18 +14,22 @@ const Image = ({ src, width, height, ...rest }: ImageProps) => (
   <NextImage src={`${basePath || ''}${src}`} height={height ?? 0} width={width ?? 600} {...rest} />
 )
 
-const SimpleImage = ({ src, width, height, ...rest }: ImgProps) => (
+const SimpleImage = ({ src, className, width, height, ...rest }: ImgProps) => (
   <picture className="flex justify-center">
     <PhotoView src={src}>
       <NextImage
         src={src}
         width={600}
         height={0}
-        className="h-auto max-h-[1000px] w-auto max-w-72 min-w-0 rounded-lg border-2 border-gray-600 sm:max-w-96 xl:max-w-xl dark:border-gray-300"
+        className={`h-auto max-h-[1000px] w-auto max-w-72 min-w-0 rounded-lg border-2 border-gray-600 sm:max-w-96 xl:max-w-xl dark:border-gray-300 ${className}`}
         {...rest}
       />
     </PhotoView>
   </picture>
 )
 
-export { Image, SimpleImage }
+const Gallery = ({ children }: { children: React.ReactNode }) => (
+  <div className="mx-auto flex max-w-6xl flex-wrap justify-center gap-4 p-4">{children}</div>
+)
+
+export { Image, SimpleImage, Gallery }
--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -2,13 +2,15 @@ import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
-import { Image, SimpleImage } from './Image'
+import { Image, SimpleImage, Gallery } from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import { AlertCard } from '@/components/Card'
 
 export const components: MDXComponents = {
   Image,
+  SimpleImage,
+  Gallery,
   TOCInline,
   a: CustomLink,
   img: SimpleImage,
