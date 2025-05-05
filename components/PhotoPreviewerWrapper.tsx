214c4d3184548a35067961f32b664f59d9465788
Alex3236 <me@alex3236.moe>
2025-05-05 13:45:10 +0800

    feat: replace react-image-previewer with react-photo-view and update styling

--- components/PhotoPreviewerWrapper.tsx
+++ components/PhotoPreviewerWrapper.tsx
@@ -1,6 +1,16 @@
 'use client'
 
-import { PhotoProvider, PhotoView } from 'react-image-previewer'
+import { PhotoProvider as Provider, PhotoView } from 'react-photo-view'
+import 'react-photo-view/dist/react-photo-view.css'
+import { ReactNode } from 'react'
+
+const PhotoProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <Provider maskOpacity={0.5} photoClosable={true}>
+      {children}
+    </Provider>
+  )
+}
 
 // make the component client side
 export { PhotoProvider, PhotoView }
