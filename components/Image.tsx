5d21ed62d696b7af3088bc354bae1ada2083ca19
Alex3236 <me@alex3236.moe>
2025-05-05 13:14:55 +0800

    feat: improve dark mode styling for images

--- components/Image.tsx
+++ components/Image.tsx
@@ -22,12 +22,12 @@ const SimpleImage = ({ src, className, width, height, alt, ...rest }: ImgProps) 
         alt={alt && alt.startsWith('#') ? alt.substring(1) : alt}
         width={600}
         height={0}
-        className={`h-auto max-h-[1000px] w-auto max-w-72 min-w-0 rounded-lg border-2 border-gray-600 sm:max-w-96 xl:max-w-xl dark:border-gray-300 ${className}`}
+        className={`h-auto max-h-[1000px] w-auto max-w-72 min-w-0 rounded-lg border-2 border-gray-600 sm:max-w-96 xl:max-w-xl dark:border-gray-300 dark:brightness-[.8] ${className}`}
         {...rest}
       />
     </PhotoView>
     {alt && alt.startsWith('#') && (
-      <span className="mt-2 text-center text-sm text-gray-600 dark:text-gray-300">
+      <span className="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
         {alt.substring(1)}
       </span>
     )}
